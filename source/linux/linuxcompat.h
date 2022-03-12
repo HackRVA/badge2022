839bc02c8beba537b47970aa66a299a369d82c50
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2022-03-12 14:17:46 -0500

    add missing header file

---
+++ source/linux/linuxcompat.h
@@ -0,0 +1,12 @@
+#ifndef LINUXCOMPAT_H
+#define LINUXCOMPAT_H
+
+/* username is a global externally visible */
+#define NAMESIZE 10
+
+/* for linux we can define here, otherwise it's in username.c */
+#ifdef __LINUX__
+char username[NAMESIZE] = { 0 };
+#endif
+
+#endif  // LINUXCOMPAT_H
